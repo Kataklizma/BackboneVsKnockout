0f26968d400472bb7191ad5245aaab9627922d3b
Florim Maxhuni <emri.mbiemri@gmail.com>
2013-06-02 00:07:09 +0200

    Knockout List done.

---
+++ Source/AzureBVSK/Knockout/app.js
@@ -0,0 +1,25 @@
+﻿var App = App || {};
+var AZUREClient = new WindowsAzure.MobileServiceClient('https://beans.azure-mobile.net/', 'PzOgnOzLrLxdRmTcMawYYxkBZdvxXo80');
+
+$(document).ready(function () {
+    var tbl = AZUREClient.getTable('FiVatCategory');
+    var categoryViewModel = function () {
+        var self = this;
+        this.items = ko.observableArray();
+        
+        self.getCategories = function (val) {
+            tbl.read({ VATCategoryNameEn: val }).done(
+              function (d) {
+                  self.items(d);
+              });
+        };
+        this.searchText = ko.observable();
+        
+        this.searchCategory = ko.computed(this.searchText).extend({ throttle: 700 });
+
+        this.searchCategory.subscribe(function (val) {
+            self.getCategories(val);
+        }, this);
+    }.bind(this);
+    ko.applyBindings(new categoryViewModel());
+});
